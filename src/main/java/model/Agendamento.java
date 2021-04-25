a94f09097f233beaa809b9da0005fae8df33fabe
srpulga <your_email_address@example.com>
2021-04-25 16:17:11 -0300

    getters and setters agendamento

--- src/main/java/model/Agendamento.java
+++ src/main/java/model/Agendamento.java
@@ -9,4 +9,43 @@ public class Agendamento {
     private ArrayList<TipoServico> tipoServicos;
     private Cliente cliente;
     private Funcionario funcionario;
+
+    public Timestamp getDataHoraInicio() {
+        return dataHoraInicio;
+    }
+
+    public void setDataHoraInicio(Timestamp dataHoraInicio) {
+        this.dataHoraInicio = dataHoraInicio;
+    }
+
+    public ArrayList<TipoServico> getTipoServicos() {
+        return tipoServicos;
+    }
+
+    public void setTipoServicos(ArrayList<TipoServico> tipoServicos) {
+        this.tipoServicos = tipoServicos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    @Override
+    public String toString() {
+        return "Agendamento{" + "dataHoraInicio=" + dataHoraInicio + ", tipoServicos=" + tipoServicos + ", cliente=" + cliente + ", funcionario=" + funcionario + '}';
+    }
+    
+    
 }
